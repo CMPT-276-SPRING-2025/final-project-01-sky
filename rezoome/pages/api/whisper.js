c108d0b38affa946fbcf19ec2167e3b8f651754e
Mara Liwayway David <davidmaraliwayway@gmail.com>
2025-03-10 17:08:08 -0700

    chore: setup openai api handling routes and edit website metadata

---
+++ rezoome/pages/api/whisper.js
@@ -0,0 +1,14 @@
+// openai whisper api handling route
+
+// load environment variables
+require('dotenv').config();
+
+import { OpenAI } from 'openai'; 
+
+export default async function handler(req, res) {
+  // initialize OpenAI with the API key from environment variables
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+}
