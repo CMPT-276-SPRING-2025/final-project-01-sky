b6f602a321a6910e54628b765ea3b8af4c46f18e
YourGitHubUsername <your_email@example.com>
2025-03-13 23:12:46 -0700

    Made cards into components and fixed padding around cards

---
+++ rezoome/components/ReasonCard.js
@@ -0,0 +1,14 @@
+// component for cards that display reason
+export default function ReasonCard({ title, description, image }) {
+    return (
+        <div className="bg-white shadow-lg rounded-lg w-[303.33px] h-[295.27px] flex flex-col p-4">
+            <img src={image} alt={title} className="w-full h-48 object-cover rounded-lg" />
+            <div className="flex flex-col justify-between mt-4">
+                <h2 className="text-xl font-semibold">{title}</h2>
+                <p className="text-base text-[var(--text-colour)] mt-2">
+                    {description}
+                </p>
+            </div>
+        </div>
+    );
+}
