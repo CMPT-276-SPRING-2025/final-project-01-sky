345b29cd8155a419b214b2cd9b07f7b7d29f6457
Colton Chan <coltonlhchan@gmail.com>
2025-03-27 22:07:27 -0700

    Set up jest for nextjs

---
+++ rezoome/jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1", // Support absolute imports
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
